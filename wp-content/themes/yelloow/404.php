8214ba745d4e9ee2f4d7c667bf2240d314d80493
Fathur Rohman <fathur_rohman17@yahoo.co.id>
2015-02-09 20:25:21 +0000

    404.php created online with Bitbucket

---
+++ wp-content/themes/yelloow/404.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+<div class="jumbotron yellow">
+	<div class="container text-center troubleshoot">
+        <div class="col-sm-12">
+		    <p>404</p>
+        </div>
+	</div>
+</div>
+
+<div class="container-fluid white">
+	<div class="container content">
+		<div class="row text-center">
+			<div class="col-sm-12">
+                <p>Page Not Found</p>
+			</div>
+		</div>
+	</div>
+</div>
+<?php get_footer(); ?>
